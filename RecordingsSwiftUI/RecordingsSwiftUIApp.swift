aadcb09351109c29a145fb3f5e7610f72bc0006b
Sunny <sundet.kz@gmail.com>
2024-10-19 09:09:03 +0500

    Initial Commit

---
+++ RecordingsSwiftUI/RecordingsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecordingsSwiftUIApp.swift
+//  RecordingsSwiftUI
+//
+//  Created by Sundet Mukhtar on 19.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct RecordingsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
